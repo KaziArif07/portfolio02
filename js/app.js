eb880f7e31b319b81fdf8f7c9ef9f9b0a3f9a8b7
Kazi Arif <70338876+KaziArif07@users.noreply.github.com>
2022-01-14 15:10:04 +0600

    update jan14
    update

--- js/app.js
+++ js/app.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
 
     });
 
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobileNav');
+
+    hamberger.addEventListener('click', function(){
+        mobileNav.classList.add('open');
+
+    })
+
+    times.addEventListener('click', function(){
+        mobileNav.classList.remove('open');
+    })
+
 });
